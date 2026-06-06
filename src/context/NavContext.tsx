2db268d040774c777a5da7c146ca6c34b8e4de7a
deepak <deepakmehra7830@gmail.com>
2026-06-06 23:17:46 +0530

    completed

--- src/context/NavContext.tsx
+++ src/context/NavContext.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+
+
 
 type NavbarContextType = [
   boolean,
@@ -6,17 +9,30 @@ type NavbarContextType = [
 ]
 
 export const NavbarContext = createContext<NavbarContextType | null>(null)
+export const NavbarColorContext = createContext()
 
-function NavContext({children}: { children: React.ReactNode}) {
+function NavContext({ children }: { children: React.ReactNode }) {
 
-    const [navOpen, setNavOpen] = useState(false)
+  const [navColor, setNavColor] = useState('white')
+  const [navOpen, setNavOpen] = useState(false)
+  const locate = useLocation().pathname
+  console.log(locate)
+  useEffect(()=>{
+    if(locate == '/projects' || locate == '/agence'){
+      setNavColor('black')
+    }else{
+      setNavColor('white')
+    }
+  }, [locate])
 
 
   return (
     <div>
-        <NavbarContext.Provider value={[navOpen, setNavOpen]}>
-        {children}
-        </NavbarContext.Provider>
+      <NavbarContext.Provider value={[navOpen, setNavOpen]}>
+        <NavbarColorContext.Provider value={[navColor, setNavColor]}>
+          {children}
+        </NavbarColorContext.Provider>
+      </NavbarContext.Provider>
     </div>
   )
 }
