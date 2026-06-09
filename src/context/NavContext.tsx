84eda95925a808178076baf1f2238529b6b30b88
deepak <deepakmehra7830@gmail.com>
2026-06-10 01:51:37 +0530

    some typescript error or npm run build

--- src/context/NavContext.tsx
+++ src/context/NavContext.tsx
@@ -8,13 +8,19 @@ type NavbarContextType = [
   React.Dispatch<React.SetStateAction<boolean>>
 ]
 
+type NavbarColorContextType = [
+  string,
+  React.Dispatch<React.SetStateAction<string>>
+]
+
+
 export const NavbarContext = createContext<NavbarContextType | null>(null)
-export const NavbarColorContext = createContext<NavbarContextType | null >(null)
+export const NavbarColorContext = createContext<NavbarColorContextType | null >(null)
 
 function NavContext({ children }: { children: React.ReactNode }) {
 
   const [navColor, setNavColor] = useState('white')
-  const [navOpen, setNavOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState<boolean>(false)
   const locate = useLocation().pathname
   useEffect(()=>{
     if(locate == '/projects' || locate == '/agence'){
